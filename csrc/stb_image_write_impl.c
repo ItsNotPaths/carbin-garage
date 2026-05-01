7cd07f8a4f9e19f85be6a9089082f18164007f8f
Paths <paths@paths.place>
2026-04-30 23:23:25 -0500

    image parsing

---
+++ csrc/stb_image_write_impl.c
@@ -0,0 +1,4 @@
+/* stb_image_write single-header implementation. PNG output for the
+   Phase 2c texture decoder; BMP / JPG / TGA come along for free. */
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
